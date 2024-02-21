d04478f1638690862bff57e0c264cfc91e136c27
gukazma <gukazma@proton.me>
2024-02-21 18:26:30 +0800

    ✨ feat: 添加资源文件

--- Apps/DemoApp/Program.cpp
+++ Apps/DemoApp/Program.cpp
@@ -1,27 +1,157 @@
-#include <qapplication.h>
+/*
+    Copyright (c) 2024 Anthony J. Thibault
+    This software is licensed under the MIT License. See LICENSE for more details.
+*/
+
+// 3d gaussian splat renderer
+
+#include <GL/glew.h>
 #include <SDL.h>
+#include <SDL_opengl.h>
+#include <chrono>
+#include <glm/glm.hpp>
+#include <stdint.h>
+#include <thread>
 
-#include "MdiWindow.h"
-#include "MainWindow.h"
+#ifdef TRACY_ENABLE
+#    include <tracy/Tracy.hpp>
+#else
+#    define FrameMark \
+        do {          \
+        } while (0)
+#endif
 
-int main(int argc, char * argv[]) {
-	QApplication a(argc, argv); // Basics of QT, start an application
+#include "core/log.h"
+#include "core/util.h"
 
-	SDL_Init(SDL_INIT_VIDEO); // Basics of SDL, init what you need to use
+#include "app.h"
 
-	
-	//MdiWindow MdiExample; // Creating and using a QMainWindow with an Mdi Area
-	//MdiExample.show();
-	
-	MainWindow MainWinExample;	// Creating an using a QMainWindow with central SDL widget
-	MainWinExample.SDLInit();	// <- Need to run SDL setup after qt window setup
-	MainWinExample.show();
-	
-	int RetVal = a.exec();	// Most examples have this on the return, we
-							// need to have it return to a variable cause:
+// #define SOFTWARE_SPLATS
 
-	SDL_Quit();	// We can't put this statement before the exec, or after
-				// the return
+struct GlobalContext
+{
+    bool          quitting = false;
+    SDL_Window*   window   = NULL;
+    SDL_GLContext gl_context;
+};
 
-	return RetVal;
+GlobalContext ctx;
+
+int SDLCALL Watch(void* userdata, SDL_Event* event)
+{
+    if (event->type == SDL_APP_WILLENTERBACKGROUND) {
+        ctx.quitting = true;
+    }
+
+    return 1;
+}
+
+int main(int argc, char* argv[])
+{
+    Log::SetAppName("splataplut");
+    MainContext      mainContext;
+    App              app(mainContext);
+    App::ParseResult parseResult = app.ParseArguments(argc, (const char**)argv);
+    switch (parseResult) {
+    case App::SUCCESS_RESULT: break;
+    case App::ERROR_RESULT: Log::E("App::ParseArguments failed!\n"); return 1;
+    case App::QUIT_RESULT: return 0;
+    }
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_JOYSTICK) != 0) {
+        Log::E("Failed to initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    const int32_t WIDTH  = 1024;
+    const int32_t HEIGHT = 768;
+
+    // Allow us to use automatic linear->sRGB conversion.
+    SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
+
+    uint32_t windowFlags = SDL_WINDOW_OPENGL;
+    if (app.IsFullscreen()) {
+        windowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+    }
+    else {
+        windowFlags |= SDL_WINDOW_RESIZABLE;
+    }
+    ctx.window = SDL_CreateWindow(
+        "splatapult", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, windowFlags);
+
+    ctx.gl_context = SDL_GL_CreateContext(ctx.window);
+
+    SDL_GL_MakeCurrent(ctx.window, ctx.gl_context);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        Log::E("Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+
+    SDL_AddEventWatch(Watch, NULL);
+
+    if (!app.Init()) {
+        Log::E("App::Init failed\n");
+        return 1;
+    }
+
+    bool shouldQuit = false;
+    app.OnQuit([&shouldQuit]() { shouldQuit = true; });
+
+    app.OnResize([](int newWidth, int newHeight) {
+        // SDL_RenderSetLogicalSize(ctx.renderer, newWidth, newHeight);
+        //  AJT: TODO resize texture?
+    });
+
+    uint32_t frameCount = 1;
+    uint32_t frameTicks = SDL_GetTicks();
+    uint32_t lastTicks  = SDL_GetTicks();
+    while (!ctx.quitting && !shouldQuit) {
+        // update dt
+        uint32_t ticks = SDL_GetTicks();
+
+        const int FPS_FRAMES = 100;
+        if ((frameCount % FPS_FRAMES) == 0) {
+            float delta = (ticks - frameTicks) / 1000.0f;
+            float fps   = (float)FPS_FRAMES / delta;
+            frameTicks  = ticks;
+            app.UpdateFps(fps);
+        }
+        float dt  = (ticks - lastTicks) / 1000.0f;
+        lastTicks = ticks;
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            app.ProcessEvent(event);
+        }
+
+        if (!app.Process(dt)) {
+            Log::E("App::Process failed!\n");
+            return 1;
+        }
+
+        SDL_GL_MakeCurrent(ctx.window, ctx.gl_context);
+
+        int width, height;
+        SDL_GetWindowSize(ctx.window, &width, &height);
+        if (!app.Render(dt, glm::ivec2(width, height))) {
+            Log::E("App::Render failed!\n");
+            return 1;
+        }
+
+        SDL_GL_SwapWindow(ctx.window);
+
+        frameCount++;
+
+        FrameMark;
+    }
+
+    SDL_DelEventWatch(Watch, NULL);
+    SDL_GL_DeleteContext(ctx.gl_context);
+
+    SDL_DestroyWindow(ctx.window);
+    SDL_Quit();
+
+    return 0;
 }
